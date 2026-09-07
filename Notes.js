ba5ea86f6cde3b74e6eb02d59895e0f2ba40503c
meetnashi-01 <bhatiameetanshi@gmail.com>
2026-09-07 20:42:37 +0530

    first commit

---
+++ Notes.js
@@ -0,0 +1,6 @@
+// mkdir filename   --> to make new Folder
+// git init ---> it is a command used to show changes 
+// git add ---> koi ek folder add krana h
+// git add . --> multiple folders ko add karana h
+// git config --global user.name "meetanshi-01"  --> for username
+// git config --global user.email "bhatiameetanshi@gmail.com"  --> for user email
